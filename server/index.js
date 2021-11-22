e3ea40914c61c9c3522c5d883fd76b1f65b4cf5e
vuld0 <chiragsonubablani@karunya.edu.in>
2021-11-22 07:42:53 +0530

    new branch for testing the play feature in the bot

--- server/index.js
+++ server/index.js
@@ -1,24 +1,60 @@
-require('dotenv').config();
-const { Client, Intents } = require('discord.js');
+const fs = require('fs');
+const Discord = require('discord.js');
+const { Player } = require('discord-player');
+const Client = require('./client/Client');
 
-const bot = new Client({
-  intents: [
-    Intents.FLAGS.GUILDS,
-    Intents.FLAGS.GUILD_MESSAGES,
-    Intents.FLAGS.GUILD_MEMBERS,
-    Intents.FLAGS.GUILD_PRESENCES,
-    Intents.FLAGS.GUILD_VOICE_STATES,
-  ],
+// an inherited class has been created from the parent 'Client' class
+const client = new Client();
+client.commands = new Discord.Collection();
+
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.name, command);
+}
+
+console.log(client.commands);
+
+const player = new Player(client);
+
+// TODO some changes
+client.on('messageCreate', async (message) => {
+  if (message.author.bot || !message.guild) return;
+  if (!client.application.owner) await client.application.fetch();
+
+  if (
+    message.content === '!deploy' &&
+    message.author.id === client.application.owner.id
+  ) {
+    await message.guild.commands
+      .set(client.commands)
+      .then(() => {
+        message.reply('Deployed!');
+      })
+      .catch((err) => {
+        message.reply(
+          'Could not deploy commands! Make sure the bot has the application.commands permission!'
+        );
+        console.error(err);
+      });
+  }
 });
 
-require('./controller/botController')(bot);
+// TODO some changes
+client.on('interactionCreate', async (interaction) => {
+  const command = client.commands.get(interaction.commandName.toLowerCase());
 
-bot.on('ready', () => {
-  console.log(`Bot ${bot.user.tag} has logged in!`);
+  try {
+    command.execute(interaction, player);
+  } catch (error) {
+    console.error(error);
+    interaction.followUp({
+      content: 'There was an error trying to execute that command!',
+    });
+  }
 });
-// prefix for our bot command
 
-// Checking if the user has connected to a voice channel
-// const voiceChannel = message
-
-bot.login(process.env.CLIENT_TOKEN); // login to bot using token
+client.login(process.env.CLIENT_TOKEN);
